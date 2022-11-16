768937a77b7fdd49dde9df5a2bdf04dbd7fea1a7
Manthan Sondagar <manthan.sondagar@gmail.com>
2022-11-16 16:40:05 +0530

    comlete application

---
+++ webpack.dev.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const dirAssets = path.resolve(__dirname, "src/assets");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/js/index.js",
+    styles: "./src/css/style.css"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+      chunks: ["app", "styles"],
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ""
+            }
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ["raw-loader", "glslify-loader"]
+      }
+    ]
+  }
+};
